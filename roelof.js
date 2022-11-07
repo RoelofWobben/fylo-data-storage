2c0e7e70a24461a9230bbfca1f82fd7737dbc33b
RoelofWobben <r.wobben@home.nl>
2022-11-07 11:56:43 +0100

    layout ready

---
+++ roelof.js
@@ -0,0 +1,22 @@
+// Zoek de range slider 
+
+const slider = document.querySelector('.slider');
+
+
+// Zoek de tekst waar de waarde in geplaats moet worden. 
+
+const tekst = document.querySelector('.bold')
+
+// Zet de waarde in de tekst
+
+// uitzoeken hoe ik een string kan krijgen met de waardes erin en dan het plaatsen in de tekst 
+
+
+
+
+
+
+
+
+
+
