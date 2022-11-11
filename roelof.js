d7b5ed2c1f9151ca305755bb46327b42b5077287
RoelofWobben <r.wobben@home.nl>
2022-11-11 11:05:43 +0100

    dynamic working

--- roelof.js
+++ roelof.js
@@ -2,14 +2,41 @@
 
 const slider = document.querySelector('.slider');
 
-
 // Zoek de tekst waar de waarde in geplaats moet worden. 
 
-const tekst = document.querySelector('.bold')
+const tekst = document.querySelector('.bold');
 
-// Zet de waarde in de tekst
+// zoek de tekst balloon 
 
-// uitzoeken hoe ik een string kan krijgen met de waardes erin en dan het plaatsen in de tekst 
+const balloon = document.querySelector('.number')
+
+// zoek de width van de slider 
+
+const width = document.querySelector('.storage-bar').clientWidth; 
+
+// verander de tekst als de slider bewogen wordt 
+
+slider.addEventListener('input',  () => {
+    // Zet de waarde in de tekst
+    tekst.innerHTML = `${slider.value} GB `; 
+    // Zet de waarde in de balloon 
+    balloon.innerHTML = `${slider.value}`;
+    // aanpassen rode balk 
+    const width_bar = (slider.value * width) / 1000;
+    document.querySelector('.storageBarFill').style.width = width_bar + 'px';
+})
+
+// bereken de juiste width voor de lengte en verandert het als het scherm verandert
+
+/*
+window.addEventListener('resize', () => {
+    const width_bar = (slider.value * width) / 1000;
+    document.querySelector('.storageBarFill').style.width = width_bar + 'px';
+}); 
+*/ 
+
+
+
 
 
 
